450fca566c44373bc5d163efab773352069bfa88
NajaTheNaja <gregory.o.marshall@gmail.com>
2021-05-06 19:23:32 -0400

    Added folder and ai file
    read title

---
+++ Module7_v2/Module7/SalmonPlayer/SalmonPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module8
+{
+    class SalmonPlayer : IPlayer
+    {
+        public string Name => throw new NotImplementedException();
+
+        public int Index => throw new NotImplementedException();
+
+        public Position GetAttackPosition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetAttackResults(List<AttackResult> results)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartNewGame(int playerIndex, int gridSize, Ships ships)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
